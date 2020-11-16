520bc0dfe6d72b6517b29414728dc21adf2f2fd1
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-11-16 13:42:13 +0100

    working on it

--- 01-Databases/taak01-database-import/antwoorden.sql
+++ 01-Databases/taak01-database-import/antwoorden.sql
@@ -5,9 +5,12 @@ SELECT * FROM jaar2015 WHERE country="Iceland"
 
 SELECT nationality FROM `players` WHERE "chelsea"
 
-
+SELECT name FROM players chelsea
 
 SELECT name FROM players WHERE nationality = "Spain" AND "age"
+SELECT name FROM players WHERE nationality = "Spain" AND wage = 15000
+
+SELECT name AND age FROM players WHERE nationality = "Spain" AND wage = 15000
 
 
 SELECT * FROM players WHERE nationality = "Spain" AND wage = 260000;

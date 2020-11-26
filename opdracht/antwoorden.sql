c5221bf69fd0d4d3a03c954c12206a81986d0587
Darimir Mateo <darimir.mateo@student.rocva.nl>
2020-11-26 18:30:49 +0100

    klaar

--- opdracht/antwoorden.sql
+++ opdracht/antwoorden.sql
@@ -63,25 +63,27 @@ SELECT * FROM videogamesales WHERE year= 1999
 -- Opdracht 3
 SELECT na_sales FROM videogamesales WHERE genre="sports"
 -- Opdracht 4
-
+SELECT name platform FROM videogamesales WHERE publisher="nintendo" AND year= "1990 BETWEEN 2005"
 -- Opdracht 5
-
+SELECT Max(Global_Sales) As Global_Sales, name FROM videogamesales 
 -- Opdracht 6 
-
+SELECT ROUND(Global_Sales) As EU_Sales FROM videogamesales WHERE genre="puzzle"
+SELECT ROUND(Global_Sales) As Global_Sales, EU_Sales FROM videogamesales WHERE genre="puzzle"
 -- Opdracht 7 
-
+SELECT name,genre,publisher FROM videogamesales WHERE JP_Sales=532
 -- Opdracht 8
-
+SELECT Count(*) As Global_Sales FROM videogamesales WHERE publisher = "nintendo"
 -- Opdracht 9
-
+SELECT name year  FROM videogamesales WHERE genre="racing" AND publisher="nintendo"
 -- Opdracht 10
-
+SELECT AVG(na_sales) AS na_sales,EU_Sales,JP_Sales FROM videogamesales
 -- Opdracht 11
-
+DELETE FROM videogamesales WHERE publisher = "xbox" AND name="halo 2"
 -- Opdracht 12
-
+DELETE FROM videogamesales WHERE publisher = "ubisoft" AND year="2012"
 -- Opdracht 13
-
+DELETE FROM videogamesales WHERE publisher = "nintendo" AND Global_Sales <1000
 -- Opdracht 14
-
+DELETE FROM videogamesales WHERE Global_Sales <1000
 -- Opdracht 15
+DELETE FROM videogamesales WHERE year= "1997" AND NA_Sales >200000
